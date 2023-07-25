dac5a64b37030ba9a051c6cfce3d0d008c519f55
kitharvey <kitharveycaubalejo@gmail.com>
2023-07-25 16:02:21 +0800

    update cert

--- sst.config.ts
+++ sst.config.ts
@@ -12,9 +12,21 @@ export default {
 	stacks(app) {
 		app.stack(function Site({ stack }) {
 			const arn =
-				"arn:aws:acm:us-east-1:584125941279:certificate/1c487774-af0e-4c63-adce-562f1cda20bc";
+				"arn:aws:acm:us-east-1:584125941279:certificate/770bf3ef-89ae-4542-8c9e-364ce5a460bd";
 
-			const site = new AstroSite(stack, "site");
+			const site = new AstroSite(stack, "site", {
+				customDomain: {
+					isExternalDomain: true,
+					domainName: "hackernews-astro.kitharvey.cc",
+					cdk: {
+						certificate: Certificate.fromCertificateArn(
+							stack,
+							"Certificate",
+							arn
+						),
+					},
+				},
+			});
 			stack.addOutputs({
 				url: site.url,
 			});
